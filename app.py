8efb02e7fb98167b0b9cb48fd8bd8b1bb44cb5cf
BrianSumayan <134151830+BrianSumayan@users.noreply.github.com>
2023-10-16 00:36:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,26 +5,31 @@ import numpy as np
 
 def main():
 
-    st.title("Classifies the Crab and Lobster Images)")
-    st.write("This app classifies whether the image is Crab or Lobster")
-   
-    @st.cache_resource
+    st.title("Crab and Lobster Image Classifier")
+    st.write("This app classifies whether the image is a Crab or a Lobster")
+
+    @st.cache(allow_output_mutation=True)
     def load_model():
         model = tf.keras.models.load_model('weights-improvement-06-0.97.hdf5')
         return model
-    
+
     def import_and_predict(image_data, model):
-        size=(128,128)
-        image = ImageOps.fit(image_data,size, Image.LANCZOS)
+        size = (128, 128)
+        image = ImageOps.fit(image_data, size, Image.LANCZOS)
         image = np.asarray(image)
         image = image / 255.0
         img_reshape = np.reshape(image, (1, 128, 128, 3))
         prediction = model.predict(img_reshape)
         return prediction
 
-    model = load_model()
-    class_names = ["Crab", "Lobster"]
-    
+    def display_prediction(image, model, class_names):
+        st.image(image, use_column_width=True)
+        prediction = import_and_predict(image, model)
+        class_index = np.argmax(prediction)
+        class_name = class_names[class_index]
+        probability = np.max(prediction)
+        string = f"Prediction: {class_name} | Probability: {probability:.2f}"
+        st.success(string)
 
     file = st.file_uploader("Select a image of Crab or Lobster in your computer", type=["jpg", "png", "jpeg"])
 
