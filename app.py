a6f8e502f2ef3e0ff6be4d46863a3e1dada1b7d6
L-Ocampo <134071058+L-Ocampo@users.noreply.github.com>
2023-10-19 20:11:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,27 +10,7 @@ def main():
     st.write("This app classifies whether the image is a Crab or a Lobster and allows you to rate the prediction")
 
     @st.cache(allow_output_mutation=True)
-    def load_model():
-        model = tf.keras.models.load_model('weights-improvement-06-0.97.hdf5')
-        return model
 
-    def import_and_predict(image_data, model):
-        size = (128, 128)
-        image = ImageOps.fit(image_data, size, Image.LANCZOS)
-        image = np.asarray(image)
-        image = image / 255.0
-        img_reshape = np.reshape(image, (1, 128, 128, 3))
-        prediction = model.predict(img_reshape)
-        return prediction
-
-    def display_prediction(image, model, class_names, index):
-        st.image(image, use_column_width=True)
-        prediction = import_and_predict(image, model)
-        class_index = np.argmax(prediction)
-        class_name = class_names[class_index]
-        probability = np.max(prediction)
-        string = f"Prediction: {class_name} | Probability: {probability:.2f}"
-        st.success(string)
 
         rating = st.slider(f"Rate the prediction {index + 1}", 1, 5, 3, key=f"slider_{index}")
         st.write(f"You rated the prediction {index + 1}: {rating} stars")
